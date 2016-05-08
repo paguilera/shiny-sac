c30a8af2f2e787f7a63809aba5f5f6648a93939e
P Aguilera <paguilera@yahoo.com>
2016-05-08 12:16:52 -0400

    Created the basic prediction model (very simple model).

--- server.R
+++ server.R
@@ -8,17 +8,23 @@
 library(shiny)
 library(plyr)
 
-diabetesRisk <- function(glucose) {
-    glucose / 200  
+diabetesRisk <- function(age, height, waist, glucose, famhist) {
+    value.age = age / 100
+    value.wtoh = 0
+    value.glucose = glucose / 200
+    value.fam = 0
+    if (famhist == TRUE) {value.fam = 1}
+    if (waist > (height/2)) {value.wtoh = 1}
+    (value.age + value.glucose + value.fam + value.wtoh) / 2
 }
 
 shinyServer(function(input, output) {
     
     output$ageValue <- renderPrint({input$age})
-    output$weightValue <- renderPrint({input$weight})
+    output$heightValue <- renderPrint({input$height})
     output$waistValue <- renderPrint({input$waist})
     output$glucoseValue <- renderPrint({input$glucose})
     output$famhistValue <- renderPrint({input$famhist})
-    output$prediction <- renderPrint({diabetesRisk(input$glucose)})
+    output$prediction <- renderPrint({diabetesRisk(input$age, input$height, input$waist, input$glucose, input$famhist)})
 
 })
--- ui.R
+++ ui.R
@@ -15,7 +15,7 @@ shinyUI(fluidPage(
 
   inputPanel(
       sliderInput(inputId = 'age', label = 'The age of the patient', value = 40, min = 10, max = 100, step = 1),
-      sliderInput(inputId = 'weight', label = 'The weight of the patient', value = 150, min = 80, max = 500, step = 0.2),
+      sliderInput(inputId = 'height', label = 'The height of the patient in inches', value = 64, min = 58, max = 94, step = 1),
       sliderInput(inputId = 'waist', label = 'The waist measurement of the patient in inches', value = 30, min = 20, max = 60, step = 1),
       numericInput(inputId = 'glucose', label = 'Glucose mg/dl', value = 90, min = 50, max = 200, step = 5),
       checkboxInput(inputId = 'famhist', label = 'History of diabetes in family?', value = FALSE),
@@ -25,12 +25,12 @@ shinyUI(fluidPage(
       h3('Results of prediction'),
       h4('You entered'),
       verbatimTextOutput("ageValue"),
-      verbatimTextOutput("weightValue"),
+      verbatimTextOutput("heightValue"),
       verbatimTextOutput("waistValue"),
       verbatimTextOutput("glucoseValue"),
       verbatimTextOutput("famhistValue"),
       h4('Which resulted in a prediction of '),
       verbatimTextOutput("prediction"),
-      tags$footer("This prediction function is provided for illustration purposes only and is not intended for use as a substitute for trained medical examination or evaluation. For serious diabetes care, please contact your primary care physician or other medical professional.")
+      tags$footer("Disclaimer: This prediction function is provided for illustration purposes only and is not intended for use as a substitute for trained medical examination or evaluation. For serious diabetes care, please contact your primary care physician or other medical professional.")
   )
 ))
